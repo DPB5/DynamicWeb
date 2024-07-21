ce58bded69383dc1a858aded7c584a17d46eeab4
DaCodeMaster <108580850+DPB5@users.noreply.github.com>
2024-07-21 14:05:43 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
+//reviewer told me to add comments so yeah
 function smoothScrollToSection(sectionNumber) {
-    const sectionId = 'section' + sectionNumber; 
-    const section = document.getElementById(sectionId);
+  // create an ID for the 
+  const sectionId = 'section' + sectionNumber; 
+  // find the section
+  const section = document.getElementById(sectionId);
   
-    if (section) { 
-      section.scrollIntoView({ behavior: 'smooth' });
-    } else {
-      console.error('Section not found:', sectionId);
-    }
+  // if the section is actually there found
+  if (section) { 
+    // smooth scroll to section
+    section.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    // in case it was not found
+    console.error('Section not found:', sectionId);
   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.main__hero');
+  const headerPosition = header.getBoundingClientRect();     
   
-  document.addEventListener('DOMContentLoaded', () => {
-      const header = document.querySelector('.main__hero');
-      const headerPosition = header.getBoundingClientRect();        
-      
-      
-     const commentForm = document.getElementById('comment-form');
+  const commentForm = document.getElementById('comment-form');
   const commentsContainer = document.getElementById('comments-container');
-  
+//from line 19 to 23 are variables
   commentForm.addEventListener('submit', (event) => {
     event.preventDefault();
-  
+    
+    // user will put here name email and comment
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const comment = document.getElementById('comment').value;
-  
+    
+    // container for the comment
     const newComment = document.createElement('div');
     newComment.classList.add('comment-container');
     newComment.innerHTML = `
@@ -31,9 +38,12 @@ function smoothScrollToSection(sectionNumber) {
       <b>Email:</b> ${email}<br>
       <b>Comment:</b> ${comment}
     `;
-  
+    
+    // put the comment in the comments container
     commentsContainer.appendChild(newComment);
+
+      // Clear input 
+      commentForm.reset()
   });
-  
-  });
-  
+});
+//mission successful 
